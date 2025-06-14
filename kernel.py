b7a9053a0b82b327c649235695fc949280d597eb
Nathan Chow <134994761+nchowcode@users.noreply.github.com>
2025-06-13 18:19:31 -0700

    add: matt code

--- kernel.py
+++ kernel.py
@@ -7,53 +7,22 @@ from collections import deque
 # PID is just an integer, but it is used to make it clear when a integer is expected to be a valid PID.
 PID = int
 
-# This class represents the MMU of the simulation.
-# The simulator will create an instance of this object and use it to translate memory accesses.
-# DO NOT modify the name of this class or remove it.
-class MMU:
-	# Called before the simulation begins (even before kernel __init__).
-	# Use this function to initialize any variables you need throughout the simulation.
-	# DO NOT rename or delete this method. DO NOT change its arguments.
-    def __init__(self, logger):
-        self.process_segments = {}  # imageine (v_start, p_start, offset)
-        self.logger = logger
-
-    # Translate the virtual address to its physical address.
-	# If it is not a valid address for the given process, return None which will cause a segmentation fault.
-	# If it is valid, translate the given virtual address to its physical address.
-	# DO NOT rename or delete this method. DO NOT change its arguments.
-    def translate(self, address: int, pid: PID) -> int | None:
-        if pid not in self.process_segments:
-            return None
-        
-        v_start, p_start, size = self.process_segments[pid]
-    
-        if address < v_start or address >= v_start + size:
-            return None
-        
-        offset = address - v_start
-        return p_start + offset
-    
-    def allocate_mem_segment(self, pid: PID, p_start: int, size: int):
-        v_start = 0x20000000
-        self.process_segments[pid] = (v_start, p_start, size)
-    
-    def deallocate_mem_segment(self, pid: PID):
-        if pid in self.process_segments:
-            del self.process_segments[pid]
-
 # This class represents the PCB of processes.
 # It is only here for your convinience and can be modified however you see fit.
 class PCB:
     pid: PID
     priority: int
     process_type: str
+    memory_start: int
+    memory_limit: int
 
     def __init__(self, pid: PID, priority: int = 0, process_type: str = "Foreground"):
         self.pid = pid
         self.priority = priority
         self.process_type = process_type
         self._saved_quantum = None
+        self.memory_start = -1
+        self.memory_limit = 0
 
 # This class represents the Kernel of the simulation.
 # The simulator will create an instance of this object and use it to respond to syscalls and interrupts.
@@ -64,10 +33,7 @@ class Kernel:
     waiting_queue: deque[PCB]
     running: PCB
     idle_pcb: PCB
-    
-    # Called before the simulation begins.
-    # Use this function to initilize any variables you need throughout the simulation.
-    # DO NOT rename or delete this method. DO NOT change its arguments.
+
     def __init__(self, scheduling_algorithm: str, logger, mmu: "MMU", memory_size: int):
         self.scheduling_algorithm = scheduling_algorithm
         self.ready_queue = deque()
@@ -75,84 +41,49 @@ class Kernel:
         self.idle_pcb = PCB(0)
         self.running = self.idle_pcb
         self.logger = logger
-        self.mmu = mmu
-        self.memory_size = memory_size
 
         self.quantum = 40
         self.time = 0
         self.semaphores = {}
         self.mutexes = {}
         
-        # new variables for Multilevel Scheduling
         self.foreground_queue = deque()  
         self.background_queue = deque()
-        self.current_queue = "foreground" # switch between foreground and background
+        self.current_queue = "foreground"
         self.queue_switch_time = 0 
         self.rr_time = 0
         self.rr_remaining_time = 0
-        self.process_start_time = 0  # Track when current process started running
-        
-        self.kernel_reserved_bytes = 10485760
-        self.available_holes = [(self.kernel_reserved_bytes, memory_size - self.kernel_reserved_bytes)]
-        self.allocated_segments = {}
+        self.process_start_time = 0
 
-    def _find_best_fit(self, memory_needed: int) -> int | None:
-        best_hole = None
-        best_index = -1
-        
-        for i, (start, size) in enumerate(self.available_holes):
+        self.mmu = mmu
+        self.mmu.kernel = self
+        self.memory_size = memory_size
+        self.kernel_memory = 10485760
+        self.free_memory = [(self.kernel_memory, self.memory_size - self.kernel_memory)]
+        self.process_memory = {}
+
+    def new_process_arrived(self, new_process: PID, priority: int, process_type: str, memory_needed: int) -> PID:
+        best_fit_hole = None
+        for start, size in self.free_memory:
             if size >= memory_needed:
-                if best_hole is None or size < best_hole[1] or (size == best_hole[1] and start < best_hole[0]):
-                    best_hole = (start, size)
-                    best_index = i
+                if best_fit_hole is None or size < best_fit_hole[1]:
+                    best_fit_hole = (start, size)
         
-        if best_hole is None:
-            return None
-        
-        start, size = best_hole
-        self.available_holes.pop(best_index)
+        if best_fit_hole is None:
+            return -1
+
+        start, size = best_fit_hole
+        self.free_memory.remove(best_fit_hole)
         
         if size > memory_needed:
-            remaining_start = start + memory_needed
-            remaining_size = size - memory_needed
-            self.available_holes.append((remaining_start, remaining_size))
-            self.available_holes.sort(key=lambda x: x[0])
-        
-        return start
+            self.free_memory.append((start + memory_needed, size - memory_needed))
+            self.free_memory.sort()
 
-    def _free_memory(self, pid: PID):
-        if pid not in self.allocated_segments:
-            return
-        
-        start, size = self.allocated_segments[pid]
-        del self.allocated_segments[pid]
-        
-        self.available_holes.append((start, size))
-        self.available_holes.sort(key=lambda x: x[0])
-        
-        merged_holes = []
-        for start, size in self.available_holes:
-            if merged_holes and merged_holes[-1][0] + merged_holes[-1][1] == start:
-                prev_start, prev_size = merged_holes[-1]
-                merged_holes[-1] = (prev_start, prev_size + size)
-            else:
-                merged_holes.append((start, size))
-        
-        self.available_holes = merged_holes
-
-    # This method is triggered every time a new process has arrived.
-    # new_process is this process's PID.
-    # priority is the priority of new_process.
-    # DO NOT rename or delete this method. DO NOT change its arguments.
-    def new_process_arrived(self, new_process: PID, priority: int, process_type: str, memory_needed: int = 0) -> PID:
-        if memory_needed > 0:
-            p_start = self._find_best_fit(memory_needed)
-            if p_start is None: 
-                return -1 # no space
-            self.allocated_segments[new_process] = (p_start, memory_needed)
-            self.mmu.allocate_mem_segment(new_process, p_start, memory_needed)
-        
         new_pcb = PCB(new_process, priority, process_type)
+        new_pcb.memory_start = start
+        new_pcb.memory_limit = memory_needed
+        self.process_memory[new_process] = {'start': start, 'limit': memory_needed}
+
 
         if self.scheduling_algorithm == "FCFS":
             self.ready_queue.append(new_pcb)
@@ -195,24 +126,50 @@ class Kernel:
 
         return self.running.pid
 
-    # This method is triggered every time the current process performs an exit syscall.
-    # DO NOT rename or delete this method. DO NOT change its arguments.
     def syscall_exit(self) -> PID:
-        self._free_memory(self.running.pid)
-        self.mmu.deallocate_mem_segment(self.running.pid)
-        
+        exiting_pid = self.running.pid
+        if exiting_pid in self.process_memory:
+            mem_info = self.process_memory.pop(exiting_pid)
+            freed_start, freed_size = mem_info['start'], mem_info['limit']
+            
+            new_free_memory = []
+            merged = False
+            for start, size in self.free_memory:
+                if start + size == freed_start:
+                    freed_start, freed_size = start, size + freed_size
+                    merged = True
+                elif freed_start + freed_size == start:
+                    freed_size += size
+                    merged = True
+                else:
+                    new_free_memory.append((start, size))
+
+            new_free_memory.append((freed_start, freed_size))
+            self.free_memory = sorted(new_free_memory)
+            
+            final_free_memory = []
+            if self.free_memory:
+                current_start, current_size = self.free_memory[0]
+                for i in range(1, len(self.free_memory)):
+                    next_start, next_size = self.free_memory[i]
+                    if current_start + current_size == next_start:
+                        current_size += next_size
+                    else:
+                        final_free_memory.append((current_start, current_size))
+                        current_start, current_size = next_start, next_size
+                final_free_memory.append((current_start, current_size))
+                self.free_memory = final_free_memory
+
+
         if self.scheduling_algorithm == "Multilevel":
             exit_time = self.rr_time
             self.running = self._choose_next_process_multilevel()
-            
             self.process_start_time = exit_time
         else:
             self.running = self.choose_next_process()
         
         return self.running.pid
 
-    # This method is triggered when the currently running process requests to change its priority.
-    # DO NOT rename or delete this method. DO NOT change its arguments.
     def syscall_set_priority(self, new_priority: int) -> PID:
         self.running.priority = new_priority
 
@@ -230,7 +187,6 @@ class Kernel:
 
         return self.running.pid
     
-    # custom helper function for multilevel scheduling
     def _choose_next_process_multilevel(self):
         if self.current_queue == "foreground" and len(self.foreground_queue) > 0:
             selected = self.foreground_queue.popleft()
@@ -259,10 +215,6 @@ class Kernel:
 
         return self.idle_pcb
 
-    # This is where you can select the next process to run.
-    # This method is not directly called by the simulator and is purely for your convinience.
-    # Feel free to modify this method as you see fit.
-    # It is not required to actually use this method but it is recommended.
     def choose_next_process(self):
         if self.scheduling_algorithm == "Multilevel":
             return self._choose_next_process_multilevel()
@@ -283,13 +235,9 @@ class Kernel:
             return selected
         return self.idle_pcb
 
-    # This method is triggered when the currently running process requests to initialize a new semaphore.
-    # DO NOT rename or delete this method. DO NOT change its arguments.
     def syscall_init_semaphore(self, semaphore_id: int, initial_value: int):
         self.semaphores[semaphore_id] = {"value": initial_value, "queue": deque()}
 
-    # This method is triggered when the currently running process calls p() on an existing semaphore.
-    # DO NOT rename or delete this method. DO NOT change its arguments.
     def syscall_semaphore_p(self, semaphore_id: int) -> PID:
         sem = self.semaphores[semaphore_id]
         if sem["value"] > 0:
@@ -301,8 +249,6 @@ class Kernel:
                 self.time = 0
         return self.running.pid
 
-    # This method is triggered when the currently running process calls v() on an existing semaphore.
-    # DO NOT rename or delete this method. DO NOT change its arguments.
     def syscall_semaphore_v(self, semaphore_id: int) -> PID:
         sem = self.semaphores[semaphore_id]
         if sem["queue"]:
@@ -332,13 +278,9 @@ class Kernel:
             sem["value"] += 1
         return self.running.pid
 
-    # This method is triggered when the currently running process requests to initialize a new mutex.
-    # DO NOT rename or delete this method. DO NOT change its arguments.
     def syscall_init_mutex(self, mutex_id: int):
         self.mutexes[mutex_id] = {"locked": False, "owner": None, "queue": deque()}
 
-    # This method is triggered when the currently running process calls lock() on an existing mutex.
-    # DO NOT rename or delete this method. DO NOT change its arguments.
     def syscall_mutex_lock(self, mutex_id: int) -> PID:
         mtx = self.mutexes[mutex_id]
         if not mtx["locked"]:
@@ -351,8 +293,6 @@ class Kernel:
                 self.time = 0
         return self.running.pid
 
-    # This method is triggered when the currently running process calls unlock() on an existing mutex.
-    # DO NOT rename or delete this method. DO NOT change its arguments.
     def syscall_mutex_unlock(self, mutex_id: int) -> PID:
         mtx = self.mutexes[mutex_id]
         if mtx["owner"] == self.running:
@@ -385,10 +325,6 @@ class Kernel:
                 mtx["owner"] = None
         return self.running.pid
 
-    # This function represents the hardware timer interrupt.
-    # It is triggered every 10 microseconds and is the only way a kernel can track passing time.
-    # Do not use real time to track how much time has passed as time is simulated.
-    # DO NOT rename or delete this method. DO NOT change its arguments.
     def timer_interrupt(self) -> PID:
         if self.scheduling_algorithm == "RR":
             self.time += 10
@@ -431,3 +367,29 @@ class Kernel:
                 self.running = self._choose_next_process_multilevel()
             
         return self.running.pid
+
+# This class represents the MMU of the simulation.
+# The simulator will create an instance of this object and use it to translate memory accesses.
+# DO NOT modify the name of this class or remove it.
+class MMU:
+    def __init__(self, logger):
+        self.logger = logger
+        self.kernel = None
+
+    def translate(self, address: int, pid: PID) -> int | None:
+        virtual_base = 0x20000000
+        if address < virtual_base:
+            return None
+
+        if pid not in self.kernel.process_memory:
+            return None
+
+        mem_info = self.kernel.process_memory[pid]
+        physical_base = mem_info['start']
+        limit = mem_info['limit']
+
+        offset = address - virtual_base
+        if offset >= limit:
+            return None
+            
+        return physical_base + offset
